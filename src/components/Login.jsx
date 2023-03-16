b76ecd002cd730408619d528773791cd1d1983ac
“Raizo-n” <sanpincha0826@gmail.com>
2023-03-15 23:50:11 -0700

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="login w-screen h-screen bg-slate-100 m-0 p-0 ">
+    <div className="login w-screen h-screen bg-slate-100 m-0 p-0">
       <div className="flex justify-center h-4/5 w-4/5 mx-auto">
-        <div className="left w-1/2 bg-blue-400">
+        {/* left */}
+        <div className="w-2/5 bg-blue-300">
           <h3 className="">aaa</h3>
         </div>
-
-        <div className="right w-1/2 bg-gray-400">
-          <div className="bg-white flex justify-center h-4/5 w-4/5">
+        {/* right */}
+        <div className="w-3/5 bg-gray-200 flex justify-center items-center">
+          <div className="bg-white flex justify-center items-center h-4/5 w-4/5">
             <div className="bg-white flex flex-col justify-center h-4/5 w-4/5 gap-5">
               <p className="text-center">Welcome SPEAKTRUTH</p>
               <h3 className="text-3xl font-bold text-center">Login</h3>
-
-              <div className="form flex flex-col justify-start gap-5">
+              {/* Form */}
+              <div className="flex flex-col justify-start gap-5">
                 <div className="flex flex-col justify-start gap-2">
                   <label className="text-left">Username</label>
-                  <input type="text" className="border"/>
+                  <input type="text" className="border" />
                 </div>
                 <div className="flex flex-col justify-center gap-2">
                   <label htmlFor="">Email</label>
@@ -31,9 +32,9 @@ const Login = () => {
                   Login
                 </button>
                 <p className="text-center">
-                  You don't have an account? {''}
+                  You don't have an account? {""}
                   <a href="" className="underline text-blue-500">
-                     SignUp
+                     Sign Up
                   </a>
                 </p>
               </div>
