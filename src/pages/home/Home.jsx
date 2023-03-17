71e4c4d132dfe2cf4df9fa4f5d73b402cf5a6028
Naoto Tokoyoda <tkyd89@gmail.com>
2023-03-17 15:54:08 -0700

    this is test

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,55 @@
+// library
+import React from "react"
+import { Link } from "react-router-dom";
+
+
+// components
+import TopHeader from "../../components/topHeader/TopHeader.jsx"
+
+// css
+import "./home.css"
+import Meeting from "../../assets/homeImages/meeting.jpeg"
+
+
+
+
+const Home = () => {
+
+  
+
+
+  return (
+    <div className="home"  >
+      
+      <TopHeader />
+
+      <div className="homeSectionWrapper">
+
+        {/* box1 */}
+        <div className="homeLeftSection">
+          <div className="homeIntrodution">
+            <span>WHISTLE BLOWER SECURITY PLATFORM</span>
+            <h2>SPEAKTRUTH: The Forefront of Secure and Anonymous Reporting.</h2>
+            <h3>Leading whistleblowing software with expert implementation support.</h3>
+          </div>
+          <Link to='/' className="topHeader_link">
+              <span className='topHeaderReport_link'>Report</span>
+          </Link>
+        </div>
+
+        {/* box2 */}
+        <div className="homeRightSection">
+          <div className="homeRightImageWrapper">
+            <img src={Meeting} alt="meeting" />
+          </div>
+        </div>
+      </div>
+
+      <h3>SPEAKTRUTH provides support for a variety of workplace, educational, and cultural settings.</h3>
+      
+      
+    </div>
+  )
+}
+
+export default Home
