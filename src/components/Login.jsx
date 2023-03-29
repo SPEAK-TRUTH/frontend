6d8119319dca12553628b4ca7c4508674199b583
“Raizo-n” <sanpincha0826@gmail.com>
2023-03-28 23:04:30 -0700

    fix: change the bg-style on the mobile

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ import BG_IMG from "../assets/images/listen-gfd3aad7e6_1920.jpeg";
 const Login = () => {
   return (
     <div className="w-screen h-screen bg-slate-100 m-0 p-0 relative">
-        <img src={BG_IMG} alt="" className="object-cover w-screen h-screen " />
+      <img src={BG_IMG} alt="" className="object-cover w-screen h-screen " />
       <div className="flex w-screen h-screen justify-center items-center px-6 md:px-12 lg:px-36 mx-auto top-0 left-0 absolute">
         {/* left */}
         <div className="hidden relative md:block bg-[#27A2DB] md:w-2/5 md:h-4/5 h-auto">
@@ -16,8 +16,13 @@ const Login = () => {
           />
         </div>
         {/* right */}
-        <div className="w-full md:w-3/5 md:h-4/5 h-auto py-6 bg-[#D9D9D9] flex justify-center items-center container">
-          <div className="bg-white flex justify-center py-6 items-center shadow-xl shadow-gray-500 h-4/5 lg:w-3/5 w-4/5 container">
+        <div className="w-full md:w-3/5 md:h-4/5 h-auto pt-28 pb-10 sm:py-9 lg:py-0 sm:bg-[#D9D9D9] bg-[#27A2DB] flex justify-center items-center container relative">
+          <img
+            src={LOGO}
+            alt="logo"
+            className="sm:hidden inline-block object-cover absolute w-2/3 top-10 left-18"
+          />
+          <div className="bg-white flex justify-center py-10 items-center shadow-xl shadow-gray-500 h-4/5 lg:w-3/5 w-4/5 container">
             <div className="flex flex-col justify-center h-full w-4/5 gap-5 max-w-[300px] container">
               <p className="text-sm md:text-base">Welcome to SPEAKTRUTH</p>
               <h3 className="text-3xl font-bold">Login</h3>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      // backgroundImage: {
-      //   'hero-pattern': "url('/assets/topHeaderImages/speaktruthLogo.png')",
-      // },
-      colors: {
-        primary: {
-          'primary': '27A2DB',
-          'bg-gray': 'D9D9D9',
-        }
-      }
-    },
   },
   plugins: [],
 }
