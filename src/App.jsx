7fc838c347fdcab1b87858b15405fdf4a6a6f0bd
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-15 14:36:58 -0700

    add: img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Login from "./components/login";
-import SignUp from "./components/SignUp";
+// import Login from "./components/login/Login";
+// import SignUp from "./components/signUp/SignUp";
+// import Confirmation from "./components/confirmation/Confirmation";
+// import CheckReport from "./components/checkReport/CheckReport";
+import AdminOverviewDashboard from "./components/adminOverviewDashboard/AdminOverviewDashboard";
+import Login2 from "./components/login/Login2";
+import SignUp2 from "./components/signUp/SignUp2";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <SignUp/>
+      {/* <Login2 /> */}
+      {/* <Confirmation /> */}
+      {/* <SignUp/> */}
+      {/* <SignUp2 /> */}
+      {/* <CheckReport /> */}
+      <AdminOverviewDashboard />
     </div>
   );
 }
