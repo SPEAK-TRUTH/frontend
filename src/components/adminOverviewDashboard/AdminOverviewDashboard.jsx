4174da41e43bbae9fa920a99b94239d6b0f3176d
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-17 03:00:31 -0700

    add: css

--- src/components/adminOverviewDashboard/AdminOverviewDashboard.jsx
+++ src/components/adminOverviewDashboard/AdminOverviewDashboard.jsx
@@ -8,7 +8,6 @@ const AdminOverviewDashboard = () => {
       <div className="container">
         {/* Header */}
         <div className="topHeaderWrapper">
-
           {/* left */}
           <div className="header-left">
             {/* <img className="logo" src={LOGO} /> */}
@@ -22,64 +21,71 @@ const AdminOverviewDashboard = () => {
         </div>
 
         {/* Side-bar */}
-        <div className="side-bar">
-            <div className="overview">Overview</div>
-            <div className="report">Report</div>
-        </div>
-
-        {/* Main Contents */}
         <div className="main-container">
+          <div className="side-bar">
+            <div className="bar-item">Overview</div>
+            <div className="bar-item">Report</div>
+          </div>
+
+          {/* Main Contents */}
+          <div className="main-wrapper">
             <div className="left">
-                <div className="content">
-                    <div className="inner-wrapper">
-                        <h3>Monthly Reports</h3>
-                        {/* graph */}
-                    </div>
+              <div className="content">
+                <div className="wrapper-gray">
+                  <div className="inner-wrapper">
+                    <h3 className="bold">Monthly Reports</h3>
+                    {/* graph */}
+                  </div>
                 </div>
+              </div>
             </div>
             <div className="right">
-                <div className="content">
-                    <div className="inner-wrapper">
-                        <h3>By department</h3>
-                        <div className="item">
-                            <p>Marketing</p>
-                            <p>7</p>
-                        </div>
-                        <div className="item">
-                            <p>Operation</p>
-                            <p>6</p>
-                        </div>
-                        <div className="item">
-                            <p>IT</p>
-                            <p>4</p>
-                        </div>
+              <div className="content">
+                <div className="wrapper-gray">
+                  <div className="inner-wrapper">
+                    <h3 className="bold">By department</h3>
+                    <div className="item">
+                      <p>Marketing</p>
+                      <p>7</p>
                     </div>
-                </div>
-                <div className="content">
-                    <div className="inner-wrapper">
-                        <h3>By Category</h3>
-                        <div className="item">
-                            <p>Harassment</p>
-                            <p>7</p>
-                        </div>
-                        <div className="item">
-                            <p>Safety</p>
-                            <p>6</p>
-                        </div>
-                        <div className="item">
-                            <p>Theft</p>
-                            <p>4</p>
-                        </div>
+                    <div className="item">
+                      <p>Operation</p>
+                      <p>6</p>
                     </div>
+                    <div className="item">
+                      <p>IT</p>
+                      <p>4</p>
+                    </div>
+                  </div>
                 </div>
+              </div>
+              <div className="content">
+                <div className="wrapper-gray">
+                  <div className="inner-wrapper">
+                    <h3 className="bold">By Category</h3>
+                    <div className="item">
+                      <p>Harassment</p>
+                      <p>7</p>
+                    </div>
+                    <div className="item">
+                      <p>Safety</p>
+                      <p>6</p>
+                    </div>
+                    <div className="item">
+                      <p>Theft</p>
+                      <p>4</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
 
         {/* Footer */}
         <div className="footer">
           <p className="footer-text">SPEAKTRUTH 2023. ALL RIGHTS RESERVED.</p>
         </div>
-
       </div>
     </>
   );
