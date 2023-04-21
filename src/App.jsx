4c9dcd2548bf8773ecd5691301a180f3a94161bd
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-21 00:30:50 -0700

    fix small bags

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Login from "./components/login/Login";
-import SignUp from "./components/signUp/SignUp";
 import Confirmation from "./components/confirmation/Confirmation";
 import CheckReport from "./components/checkReport/CheckReport";
-import SignUp2 from "./components/signUp/SignUp2";
 
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
       {/* <Confirmation /> */}
-      {/* <SignUp/> */}
-      <SignUp2 />
-      {/* <CheckReport /> */}
+      <CheckReport />
     </div>
   );
 }
