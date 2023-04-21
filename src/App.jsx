1cb5692e980e6c086e94f853ac240515ca9beabd
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-20 23:03:07 -0700

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import UserReport from "./pages/userReport/UserReport.jsx";
 
-// import Login from "./components/login";
-// import SignUp2 from "./components/SignUp2";
-
 // css
 import "./App.css"
 
