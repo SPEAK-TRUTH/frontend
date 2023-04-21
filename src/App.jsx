ac9007a7f86a132083b9c88f98483d8874cc22f6
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-21 00:54:17 -0700

    fix small bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Confirmation from "./components/confirmation/Confirmation";
 import CheckReport from "./components/checkReport/CheckReport";
 
 function App() {
   return (
     <div className="App">
-      {/* <Confirmation /> */}
-      <CheckReport />
+      < CheckReport />
     </div>
   );
 }
